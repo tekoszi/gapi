047d43c9a6c5d345cc62668809a1d3682c17c897
tekoszi <tekoszi@hotmail.com>
2020-12-04 11:37:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### This is a Geolocalization REST API
+#
+
+This api enables you to add, delete or provide geolocation data on the base of ip address or URL.
+ 
---
+++ api/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import os
+import markdown
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    with open(os.path.dirname(app.root_path) +'\README.md', 'r') as file:
+        content = file.read()
+        return markdown.markdown(content)
