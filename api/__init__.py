10110e5be126f352a37d78fcaa8d1f5ed0b2a77b
tekoszi <tekoszi@hotmail.com>
2020-12-04 16:19:27 +0100

    adding route

--- api/__init__.py
+++ api/__init__.py
@@ -9,3 +9,10 @@ def index():
     #     content = file.read()
     #     return markdown.markdown(content)
     return 'Hello World'
+
+@app.route("/add")
+def index():
+    # with open(os.path.dirname(app.root_path) +'\README.md', 'r') as file:
+    #     content = file.read()
+    #     return markdown.markdown(content)
+    return 'add Hello World'
