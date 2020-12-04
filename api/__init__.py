9ddcb9ca17929228beeb8b6d3144cf0693094373
tekoszi <tekoszi@hotmail.com>
2020-12-04 16:27:00 +0100

    route commit

--- api/__init__.py
+++ api/__init__.py
@@ -3,15 +3,38 @@ import os
 import markdown
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
+    with open(os.path.dirname(app.root_path) +'\README.md', 'r') as file:
+        content = file.read()
+        return markdown.markdown(content)
+
+@app.route("/create")
+def create():
     # with open(os.path.dirname(app.root_path) +'\README.md', 'r') as file:
     #     content = file.read()
     #     return markdown.markdown(content)
-    return 'Hello World'
+    return 'add Hello World'
 
-@app.route("/add")
-def add():
+
+@app.route("/read")
+def read():
+    # with open(os.path.dirname(app.root_path) +'\README.md', 'r') as file:
+    #     content = file.read()
+    #     return markdown.markdown(content)
+    return 'add Hello World'
+
+@app.route("/update")
+def update():
+    # with open(os.path.dirname(app.root_path) +'\README.md', 'r') as file:
+    #     content = file.read()
+    #     return markdown.markdown(content)
+    return 'add Hello World'
+
+
+@app.route("/delete")
+def delete():
     # with open(os.path.dirname(app.root_path) +'\README.md', 'r') as file:
     #     content = file.read()
     #     return markdown.markdown(content)
