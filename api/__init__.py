f185ef9fc9c1094fb769076f897112856267e540
tekoszi <tekoszi@hotmail.com>
2020-12-04 18:26:36 +0100

    route commit

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask
 import os
-import markdown
-from flask_restful import Resource, Api, reqparse
 import sqlite3
+
+import markdown
+import requests
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,60 +12,83 @@ api = Api(app)
 
 @app.route("/")
 def index():
-    with open(os.path.dirname(app.root_path) +'/README.md', 'r') as file:
+    with open(os.path.dirname(app.root_path) + '/README.md', 'r') as file:
         content = file.read()
         return markdown.markdown(content)
 
 
 @app.route("/create")
 def create():
-
     return 'add Hello World'
 
 
 @app.route("/read")
 def read():
-
     return os.environ['apikey']
+
 
 @app.route("/update")
 def update():
-
     return 'add Hello World'
 
 
 @app.route("/delete")
 def delete():
-
     return 'add Hello World'
 
+
 class Geo(Resource):
+    def getIpGeo(self, ip):
+        url = f"http://api.ipstack.com/{ip}?access_key={os.environ['apikey']}"
+        response = requests.request("GET", url)
+        return response
+
+    def dbConnect(self):
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+
+        return (conn, c)
+
     def get(self):
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
         c.execute("SELECT * FROM geo")
         return {'message': 'Success', 'data': c.fetchall()}
+
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('ip', required=True)
         parser.add_argument('location', required=True)
         parser.add_argument('symbol', required=True)
+        parser.add_argument('method', required=True)
         args = parser.parse_args()
-        conn = sqlite3.connect('database.db')
-        c = conn.cursor()
-        ip = (args['ip'],)
-        c.execute('SELECT * FROM geo WHERE ip=?', ip)
-        if not c.fetchall():
-            geoData = (args['ip'], args['location'], args['symbol'])
-            c.execute('INSERT INTO geo VALUES (?,?,?)', geoData)
-            conn.commit()
-            return {'message': 'Successfully added new data', 'data': args}
-        else:
-            updateData = (args['location'], args['symbol'], args['ip'])
-            c.execute("UPDATE geo SET location=?, symbol=? WHERE ip=?", updateData)
-            conn.commit()
-            return {'message': 'Successfully updated the record', 'data': args}
+
+        conn, c = self.dbConnect()
+        c.execute('SELECT * FROM geo WHERE ip=?', (args['ip'],))
+
+        if args['method'] == 'manual':
+            if not c.fetchall():
+                geoData = (args['ip'], args['location'], args['symbol'])
+                c.execute('INSERT INTO geo VALUES (?,?,?)', geoData)
+                conn.commit()
+                return {'message': 'Successfully added new manual data', 'data': args}
+            else:
+                updateData = (args['location'], args['symbol'], args['ip'])
+                c.execute("UPDATE geo SET location=?, symbol=? WHERE ip=?", updateData)
+                conn.commit()
+                return {'message': 'Successfully updated the record with manual data', 'data': args}
+        elif args['method'] == 'auto':
+            ipGeoData = self.getIpGeo(args['ip'])
+            if not c.fetchall():
+                geoData = (args['ip'], ipGeoData['country_name'], ipGeoData['country_code'])
+                c.execute('INSERT INTO geo VALUES (?,?,?)', geoData)
+                conn.commit()
+                return {'message': 'Successfully added new automatic data', 'data': args}
+            else:
+                updateData = (ipGeoData['country_name'], ipGeoData['country_code'], args['ip'])
+                c.execute("UPDATE geo SET location=?, symbol=? WHERE ip=?", updateData)
+                conn.commit()
+                return {'message': 'Successfully updated the record with automatic data', 'data': args}
 
 
-api.add_resource(Geo,'/geo')
-
+api.add_resource(Geo, '/geo')
