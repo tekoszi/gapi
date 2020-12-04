90aaf51221dbc8c0a571d0e23897d3de6194c8f4
tekoszi <tekoszi@hotmail.com>
2020-12-04 19:38:17 +0100

    route commit

--- dbMaganer.py
+++ dbMaganer.py
@@ -1,6 +1,6 @@
 import jwt
 
-encoded_jwt = jwt.encode({'dominik': 'password'}, 'secret', algorithm='HS256').decode("utf-8")
+encoded_jwt = jwt.encode({"dominik": "password"}, 'secret', algorithm='HS256').decode("utf-8")
 print(encoded_jwt)
 
 print(jwt.decode(encoded_jwt.encode("utf-8"), 'secret', algorithms=['HS256']))
@@ -20,7 +20,7 @@ c = conn.cursor()
 # }
 
 # c.execute('''CREATE TABLE auth (token text)''')
-c.execute(r"INSERT INTO auth VALUES ('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21pbmlrIjoicGFzc3dvcmQifQ.qq5UOReD2zE3p3f5LDwdjedmhqGMPQb-na0c-bWLjGo')")
+# c.execute(r"INSERT INTO auth VALUES ('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21pbmlrIjoicGFzc3dvcmQifQ.qq5UOReD2zE3p3f5LDwdjedmhqGMPQb-na0c-bWLjGo')")
 # c.execute("INSERT INTO geo VALUES ('0.0.0.3','Poland','PL')")
 #
 # ip = ('0.0.0.5',)
